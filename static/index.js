ac4f266c9fcee6ee6cfe0118536c90df57f4a2cb
Mohammadreza Farashahi <101924071+rezafarashahi@users.noreply.github.com>
2023-06-22 21:49:10 +1200

    Add files via upload

---
+++ static/index.js
@@ -0,0 +1,47 @@
+// x = document.getElementById("header").innerHTML;
+// console.log(x);
+// document.write(x)
+// document.getElementById("inp").value = "hello";
+
+
+// var x = "Hello world!!";
+// console.log(x);
+//
+// function red(){
+//     console.log("red");
+//     // document.write("red")
+//     document.getElementById("header").style.color = 'red';
+// }
+// function green(){
+//     console.log("green");
+//     // document.write("red")
+//     document.getElementById("header").style.color = 'green';
+// }
+
+function press(){
+    x = document.getElementById("inp2").value;
+    document.getElementById('div2').style.color = x;
+}
+
+function printOutput(){
+    var x = document.getElementById("inp3").value;
+    // document.getElementById("p3").innerHTML = x.toUpperCase();
+    p3.innerHTML = x.toUpperCase();
+    // console.log(x)
+}
+
+// var counter = 0;
+//
+// function printNumber() {
+//   console.log(counter);
+//   counter++;
+// }
+//
+// // Call the printNumber function every 1 second
+// setInterval(printNumber, 1000);
+
+// var x = [1,2,3,4,5,6, 'tim']
+//
+// for (let el of x){
+//     console.log(el)
+// }
