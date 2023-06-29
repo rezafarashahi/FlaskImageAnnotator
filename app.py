06186bb1e28f2f5e600005102cd1129f403245f0
Mohammadreza Farashahi <gdk3673@autuni.ac.nz>
2023-06-29 18:07:43 +1200

    dataset added

--- app.py
+++ app.py
@@ -8,4 +8,5 @@ app.register_blueprint(routes, url_prefix="/")
 
 
 if __name__ == '__main__':
+
     app.run(debug=True)
--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, Blueprint, url_for
 from markupsafe import escape
 import os
+import numpy as np
 
 
 routes = Blueprint(__name__, "routes")
@@ -10,5 +11,7 @@ routes = Blueprint(__name__, "routes")
 
 @routes.route("/")
 def home():
-    args = request.args
-    return render_template("index.html")
+
+    img_list = os.listdir("dataset/pics")
+    random_img_name = img_list[np.random.randint(0, len(img_list))]
+    return render_template("index.html", img_name=random_img_name)
