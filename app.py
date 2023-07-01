202348739b29890aae30e80022c0526f00690d9a
Mohammadreza Farashahi <gdk3673@autuni.ac.nz>
2023-07-01 17:48:35 +1200

    a

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ from routes import routes
 app = Flask(__name__)
 app.register_blueprint(routes, url_prefix="/")
 
-
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
 
 if __name__ == '__main__':
 
--- routes.py
+++ routes.py
@@ -2,6 +2,10 @@ from flask import render_template, request, Blueprint, url_for
 from markupsafe import escape
 import os
 import numpy as np
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 
 
 routes = Blueprint(__name__, "routes")
@@ -15,3 +19,7 @@ def home():
     img_list = os.listdir("static/dataset/pics")
     random_img_name = img_list[np.random.randint(0, len(img_list))]
     return render_template("index.html", img_name=random_img_name)
+
+
+
+class
