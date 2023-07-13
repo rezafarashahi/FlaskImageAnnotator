c6f7b1e8199c78cf252b537fa5099e6109506e0d
Mohammadreza Farashahi <gdk3673@autuni.ac.nz>
2023-07-14 11:45:01 +1200

    a

--- create_database.py
+++ create_database.py
@@ -1,5 +1,19 @@
 from __init__ import app
 from __init__ import db
+from __init__ import Users
+from werkzeug.security import generate_password_hash
+
+
+
+
 
 app.app_context().push()
+
+
+
+u = Users(username="sara",
+          password_hash=generate_password_hash("sara1", "sha256"))
+db.session.add(u)
+db.session.commit()
+
 db.create_all()
