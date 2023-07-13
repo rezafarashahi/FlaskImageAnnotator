084ec189c3b066ab6b7fa9104e5c3ec349d646fd
Mohammadreza Farashahi <gdk3673@autuni.ac.nz>
2023-07-14 11:58:33 +1200

    a

--- __init__.py
+++ __init__.py
@@ -44,6 +44,7 @@ def load_user(user_id):
 
 # Create a model
 class Users(db.Model, UserMixin):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False)
     name = db.Column(db.String(100), nullable=True)
